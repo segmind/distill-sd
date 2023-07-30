e02924bc79a8d36f80da4e791744744678663e32
Vishnu V Jaddipal <95531133+Gothos@users.noreply.github.com>
2023-07-30 22:11:08 +0530

    Update trainT2I.py

--- trainT2I.py
+++ trainT2I.py
@@ -33,7 +33,7 @@ from diffusers.training_utils import EMAModel
 from diffusers.utils import check_min_version, deprecate, is_wandb_available
 from diffusers.utils.import_utils import is_xformers_available
 
-from BKSDM import prepare_unet
+import gc
 
 if is_wandb_available():
     import wandb
@@ -246,6 +246,40 @@ def main():
         vae = AutoencoderKL.from_pretrained(
             config.pretrained_model_name_or_path, subfolder="vae", revision=config.revision
         )
+
+    def prepare_unet(unet, model_type):
+        assert model_type in ["sd_tiny", "sd_small"]
+        # Set mid block to None if mode is other than base
+        if model_type != "sd_small":
+            unet.mid_block = None
+            
+        # Commence deletion of resnets/attentions inside the U-net
+        # Handle Down Blocks
+        for i in range(3):
+            delattr(unet.down_blocks[i].resnets, "1")
+            delattr(unet.down_blocks[i].attentions, "1")
+    
+        if model_type == "sd_tiny":
+            delattr(unet.down_blocks, "3")
+            unet.down_blocks[2].downsamplers = None
+    
+        else:
+            delattr(unet.down_blocks[3].resnets, "1")
+        # Handle Up blocks
+    
+        unet.up_blocks[0].resnets[1] = unet.up_blocks[0].resnets[2]
+        delattr(unet.up_blocks[0].resnets, "2")
+        for i in range(1, 4):
+            unet.up_blocks[i].resnets[1] = unet.up_blocks[i].resnets[2]
+            unet.up_blocks[i].attentions[1] = unet.up_blocks[i].attentions[2]
+            delattr(unet.up_blocks[i].attentions, "2")
+            delattr(unet.up_blocks[i].resnets, "2")
+        if model_type == "sd_tiny":
+            for i in range(3):
+                unet.up_blocks[i] = unet.up_blocks[i + 1]
+            delattr(unet.up_blocks, "3")
+        torch.cuda.empty_cache()
+        gc.collect()
 
     unet = UNet2DConditionModel.from_pretrained(
         config.pretrained_model_name_or_path, subfolder="unet", revision=config.non_ema_revision
